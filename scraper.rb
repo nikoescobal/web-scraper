db1e14da3935d06b335d02aebdb9a298cc3be2d8
Niko Escobal <niko.escobal@gmail.com>
2021-02-24 22:09:05 +0800

    Added get_listings method to call the scraper.

--- scraper.rb
+++ scraper.rb
@@ -10,3 +10,17 @@ class Scraper
     @name = name
     @parsed_page = Nokogiri::HTML(HTTParty.get(url).body)
   end
+
+  def get_listings
+    job_listings = @parsed_page.css('li.feature')
+    jobs = []
+    job_listings.each do |job_listing|
+      jobs << Listing.new(job_listing.css('span.company').text,
+    job_listing.css('span.title').text,
+    job_listing.css('span.company').text,
+    job_listing.css('span.region.company').text,
+    job_listing.css('a')[0].attributes["href"].value)
+    end
+    jobs
+  end
+end
